f6532b0be145687f14a721ab4598fa414b5439d0
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-10-04 09:20:46 +0530

    First version of about rajesh pillai cli package

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const log = console.log;
+
+const bio = `
+  Rajesh Pillai 
+  --------------
+  Coder and author, including many udemy and youtube courses.  Founder of a Tech Startup,
+  Algorisys Technologies, community learning platform teachyourselfcoding.com (a beta 
+  version).
+
+  20+ Years experience in the IT industry, software architect,mentor a trainer at heart.
+`;
+
+const social = `
+  https://twitter.com/rajeshpillai
+  https://github.com/rajeshpillai
+  https://teachyourself.com
+`;
+
+log(bio);
+log(chalk.green(social));
---
+++ readme.md
@@ -0,0 +1,21 @@
+# A learning project for beginner for publishing npm packages
+## First seen on nodecli.com
+
+```
+npx rajesh
+```
+
+# Usage
+To use the CLI run the following command:
+
+```
+npx rajesh
+```
+
+
+# Development
+To develop the CLI locally follow the steps
+- Download the repo
+- Run 'npm link' command on the terminal/console
+
+
